edeb75d924370497ba1d0d164e1dbe2098c30b45
Martin Albert <martin.albert.187@gmail.com>
2023-04-21 12:06:45 +0200

    Move request options methods to separate file

--- pkg/bill/client.go
+++ pkg/bill/client.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
-	"strconv"
 	"strings"
 
 	"google.golang.org/grpc/codes"
@@ -255,79 +254,4 @@ func (c *Client) doRequest(
 	}
 
 	return nil
-}
-
-// Method Apply for Credentials struct adds credentials to the request body.
-func (credentials Credentials) Apply(body *url.Values) {
-	// add username (required for login)
-	if credentials.Username != "" {
-		body.Set("userName", credentials.Username)
-	}
-
-	// add password (required for login)
-	if credentials.Password != "" {
-		body.Set("password", credentials.Password)
-	}
-
-	// add organization id (required for login)
-	if credentials.OrganizationId != "" {
-		body.Set("orgId", credentials.OrganizationId)
-	}
-
-	// add developer key (required for login)
-	if credentials.DeveloperKey != "" {
-		body.Set("devKey", credentials.DeveloperKey)
-	}
-
-	// add session id
-	if credentials.SessionId != "" {
-		body.Set("sessionId", credentials.SessionId)
-	}
-}
-
-// Method Apply for PaginationParams struct adds pagination parameters to the request body.
-func (pagination PaginationParams) Apply(body *url.Values) {
-	data := url.Values{}
-
-	// add max reference
-	if pagination.Max != 0 {
-		data.Set("max", strconv.Itoa(pagination.Max))
-	}
-
-	// add start reference
-	if pagination.Start != 0 {
-		data.Set("start", strconv.Itoa(pagination.Start))
-	}
-
-	body.Set("data", data.Encode())
-}
-
-// Method Apply for SearchParams struct adds search parameters (like id of the resource) to the request body.
-// In case of Bill.com API, it uses the data field to pass the search parameters as well as the pagination parameters
-// So this function handles both cases.
-func (searchParams SearchParams) Apply(body *url.Values) {
-	// add Id if provided
-	if searchParams.Id != "" {
-		// check if the data field is already set
-		if body.Has("data") {
-			data, err := url.ParseQuery(body.Get("data"))
-			if err != nil {
-				return
-			}
-
-			data.Set("id", searchParams.Id)
-			body.Set("data", data.Encode())
-		} else {
-			data := url.Values{}
-
-			data.Set("id", searchParams.Id)
-			body.Set("data", data.Encode())
-		}
-	}
-
-	// TODO: add filtering and sorting?
-}
-
-func IsInvalidResponse[T any](response BaseResponse[T]) bool {
-	return response.Status == 1 || response.Message == "Error"
 }
